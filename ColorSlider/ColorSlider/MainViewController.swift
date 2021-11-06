8bc23818c5690c160cee78a5356e9ca5051e2b3f
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-06 16:10:45 +0300

    hw 2.6 set mainViewController background color from SettingsViewController

--- ColorSlider/ColorSlider/MainViewController.swift
+++ ColorSlider/ColorSlider/MainViewController.swift
@@ -7,27 +7,31 @@
 
 import UIKit
 
+protocol MainViewControllerDelegate {
+    func setNewBackgroundColor(for color: UIColor)
+}
+
 class MainViewController: UIViewController {
     
     @IBOutlet var backgroundView: UIView!
     
     private let settingsSegueIdentifier = "showSettingsViewController"
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == settingsSegueIdentifier {
             guard let settingsViewController = segue.destination as? SettingsViewController else { return }
-            
+            settingsViewController.delegate = self
         }
     }
 
     @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: settingsSegueIdentifier, sender: nil)
     }
-    
-    
+}
+
+//MARK: - MainViewControllerDelegate
+extension MainViewController: MainViewControllerDelegate {
+    func setNewBackgroundColor(for color: UIColor) {
+        backgroundView.backgroundColor = color
+    }
 }
--- ColorSlider/ColorSlider/SettingsViewController.swift
+++ ColorSlider/ColorSlider/SettingsViewController.swift
@@ -13,77 +13,144 @@ private enum Constants {
 }
 
 class SettingsViewController: UIViewController {
-    @IBOutlet weak var currentColorView: UIView!
+    @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var redColorSaturationLabel: UILabel!
     @IBOutlet weak var redColorSaturationSlider: UISlider!
+    @IBOutlet weak var redColorUITextField: UITextField!
     
     @IBOutlet weak var greenColorSaturationLabel: UILabel!
     @IBOutlet weak var greenColorSaturationSlider: UISlider!
+    @IBOutlet weak var greenColorUITextField: UITextField!
     
     @IBOutlet weak var blueColorSaturationLabel: UILabel!
     @IBOutlet weak var blueColorSaturationSlider: UISlider!
+    @IBOutlet weak var blueColorUITextField: UITextField!
+    
+    var delegate: MainViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentColorView.makeRound(with: Constants.defaultViewRadius)
+        
+        setupDefaultSettings()
+        updateUI()
+    }
+    
+    // MARK: - IBAction
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        updateUI()
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        updateUI()
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        updateUI()
+    }
+    
+    @IBAction func doneButtonPressed() {
+        delegate?.setNewBackgroundColor(for: getCurrentCommonSliderColor())
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - Private Methods
+    private func setupDefaultSettings() {
+        colorView.makeRound(with: Constants.defaultViewRadius)
         
         redColorSaturationSlider.defaultConfigure()
         greenColorSaturationSlider.defaultConfigure()
         blueColorSaturationSlider.defaultConfigure()
         
-        redColorSaturationSlider.minimumTrackTintColor = .red
-        greenColorSaturationSlider.minimumTrackTintColor = .green
-        blueColorSaturationSlider.minimumTrackTintColor = .blue
-        
-        redColorSaturationLabel.text = String(redColorSaturationSlider.value)
-        greenColorSaturationLabel.text = String(greenColorSaturationSlider.value)
-        blueColorSaturationLabel.text = String(blueColorSaturationSlider.value)
-    }
-
-    @IBAction func redSliderChanged(_ sender: UISlider) {
-        setCurrentColorViewColor()
-        redColorSaturationLabel.text = String(format: Constants.sliderValueFormat,
-                                              redColorSaturationSlider.value)
-    }
-    @IBAction func doneButtonPressed() {
-        dismiss(animated: true, completion: nil)
+        redColorUITextField.delegate = self
+        greenColorUITextField.delegate = self
+        blueColorUITextField.delegate = self
     }
     
-    @IBAction func greenSliderChanged(_ sender: UISlider) {
-        setCurrentColorViewColor()
+    private func updateUI() {
+        updateSliderColor()
+        updateColorSaturationLabel()
+        updateTextField()
+        updateViewColor()
+    }
+    
+    private func getCurrentCommonSliderColor() -> UIColor {
+        return UIColor(cgColor: CGColor(
+                        red: CGFloat(redColorSaturationSlider.value),
+                        green: CGFloat(greenColorSaturationSlider.value),
+                        blue: CGFloat(blueColorSaturationSlider.value),
+                        alpha: 1))
+    }
+    
+    private func updateSliderColor() {
+        redColorSaturationSlider.minimumTrackTintColor = UIColor(cgColor: CGColor(
+                                                                    red: CGFloat(redColorSaturationSlider.value),
+                                                                    green: .zero,
+                                                                    blue: .zero,
+                                                                    alpha: 1))
+        
+        greenColorSaturationSlider.minimumTrackTintColor = UIColor(cgColor: CGColor(
+                                                                    red: .zero,
+                                                                    green: CGFloat(greenColorSaturationSlider.value),
+                                                                    blue: .zero,
+                                                                    alpha: 1))
+        
+        blueColorSaturationSlider.minimumTrackTintColor = UIColor(cgColor: CGColor(
+                                                                    red: .zero,
+                                                                    green: .zero,
+                                                                    blue: CGFloat(blueColorSaturationSlider.value),
+                                                                    alpha: 1))
+    }
+    
+    private func updateColorSaturationLabel() {
+        redColorSaturationLabel.text =  String(format: Constants.sliderValueFormat,
+                                               redColorSaturationSlider.value)
         greenColorSaturationLabel.text = String(format: Constants.sliderValueFormat,
                                                 greenColorSaturationSlider.value)
-    }
-    
-    @IBAction func blueSliderChanged(_ sender: UISlider) {
-        setCurrentColorViewColor()
         blueColorSaturationLabel.text = String(format: Constants.sliderValueFormat,
                                                blueColorSaturationSlider.value)
     }
     
-    private func setCurrentColorViewColor() {
-        currentColorView.backgroundColor = UIColor(cgColor: CGColor(
-                                                    red: CGFloat(redColorSaturationSlider.value),
-                                                    green: CGFloat(greenColorSaturationSlider.value),
-                                                    blue: CGFloat(blueColorSaturationSlider.value),
-                                                    alpha: 1))
-        
+    private func updateTextField() {
+        redColorUITextField.text =  String(format: Constants.sliderValueFormat,
+                                           redColorSaturationSlider.value)
+        greenColorUITextField.text = String(format: Constants.sliderValueFormat,
+                                            greenColorSaturationSlider.value)
+        blueColorUITextField.text = String(format: Constants.sliderValueFormat,
+                                           blueColorSaturationSlider.value)
+    }
+    
+    private func updateViewColor() {
+        colorView.backgroundColor = getCurrentCommonSliderColor()
     }
 }
 
+// MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //todo выставлять значения соответствующего слайдера и лейбла в установленное значение
+    }
+}
+
+// MARK: - UISlider
 private extension UISlider {
     
     func defaultConfigure() {
         self.minimumValue = 0.00
         self.maximumValue = 1.00
-        self.value = 0.50
+        self.value = 0.55
     }
 }
 
+// MARK: - UIView
 private extension UIView {
     
     func makeRound(with radius: CGFloat) {
         self.layer.cornerRadius = radius
     }
+}
+
+private extension CGFloat {
+    static let zero = 0.0
 }
